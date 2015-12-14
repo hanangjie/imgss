b94178b1a7c5dfc5ece6d56ef38e1cc5db981b38
hanangjie <hanangjie@gmail.com>
2015-12-14 18:05:47 +0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -54,4 +54,87 @@ router.post('/img', function(req, res, next) {
 		},3000);
 	
 });
+
+router.get('/img', function(req, res, next) {
+	var url=req.query.url;
+	res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+		res.header("X-Powered-By",' 3.2.1')
+		res.header("Content-Type", "application/json;charset=utf-8");
+	
+	http.get(url, function(res){
+	    var imgData = "";
+
+	    res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
+
+
+	    res.on("data", function(chunk){
+	        imgData+=chunk;
+	    });
+
+	    res.on("end", function(){
+	        fs.writeFile("./public/logonew.png", imgData, "binary", function(err){
+	            if(err){
+	                console.log("down fail");
+	            }
+						result("./public/logonew.png");
+	        });
+	    });
+
+	});
+
+	function result(img){
+			var fileName="logonew.png";
+			var datas = fs.readFileSync(img);
+
+			var boundary ="---------------------------leon";
+
+			var formStr = '--' + boundary 
+			+ '\r\n\r\n'
+			+ 'Content-Disposition: form-data; name="blogid"'
+			+ '\r\n\r\n'
+			//+ '测试啊'
+			+ '77601'
+			+ '\r\n'
+			+ '--' + boundary
+			+ '\r\n'
+			+ 'Content-Disposition: form-data; name="pictype"'
+			+ '\r\n\r\n'
+			+ 'ordinaryImage'
+			+ '\r\n'
+			+ '--' + boundary 
+			+ '\r\n'
+			+ 'Content-Disposition: form-data; name="file"; filename="' + encodeURIComponent(fileName) + '"'
+			+ '\r\n'
+			+ 'Content-Type: image/jpg'
+			+ '\r\n\r\n';
+
+			var formEnd = '\r\n--' + boundary + '--\r\n';
+			console.log(formStr);
+			var options = {
+				host :"120.26.67.221",
+				port : 8081,
+				method :"POST",
+				path :"/LabHomeAdmin/commonajax/fileupload.do",
+				headers : {
+				"Content-Type":"multipart/form-data; boundary="+ boundary,
+				"Content-Length": formStr.length + datas.length + formEnd.length
+				}
+			};
+
+			var req = http.request(options, function(res) {
+				res.on("data", function(data) {
+					console.log("返回数据"+ data);
+				});
+			});
+
+			req.write(formStr);
+			req.write(datas);
+			req.write(formEnd);
+			req.end();
+		}
+});
+
+
 module.exports = router;
