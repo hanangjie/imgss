2ed2409b50ce1e2a540d5122205a8eb6b61f3089
hanangjie <hanangjie@gmail.com>
2015-12-16 18:02:34 +0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var http = require("http");
 var iconv = require('iconv-lite');
 var fs = require('fs'); 
+var path=require("path"); 
+var formidable = require("formidable");//图片上传第三方包
+var util = require('util');
  
 
 /* GET home page. */
@@ -79,82 +82,177 @@ router.get('/img', function(req, res, next) {
 	            if(err){
 	                console.log("down fail");
 	            }
-						result("./public/logonew.jpg");
+						result("./public/logonew.jpg",blogid);
 	        });
 	    });
 
 	});
 
-	function result(img){
-			var fileName="logonew.jpg";
-			var datas = fs.readFileSync(img);
-
-			var boundary ="---------------------------leon";
-
-			var formStr = '--' + boundary 
-			+ '\r\n\r\n'
-			+ 'Content-Disposition: form-data; name="blogid"'
-			+ '\r\n\r\n'
-			//+ '测试啊'
-			+ blogid
-			+ '\r\n'
-			+ '--' + boundary
-			+ '\r\n'
-			+ 'Content-Disposition: form-data; name="pictype"'
-			+ '\r\n\r\n'
-			+ 'ordinaryImage'
-			+ '\r\n'
-			+ '--' + boundary 
-			+ '\r\n'
-			+ 'Content-Disposition: form-data; name="file"; filename="' + encodeURIComponent(fileName) + '"'
-			+ '\r\n'
-			+ 'Content-Type: application/octet-stream'
-			+ '\r\n\r\n';
-
-			var formEnd = '\r\n--' + boundary + '--\r\n';
-			console.log(formStr);
-			var options = {
-				host :"120.26.67.221",
-				port : 8081,
-				method :"POST",
-				path :"/LabHomeAdmin/commonajax/fileupload.do",
-				headers : {
-				"Content-Type":"multipart/form-data; boundary="+ boundary,
-				"Content-Length": formStr.length + datas.length + formEnd.length
-				}
-			};
-			options = {
-				host :"localhost",
-				port : 3003,
-				method :"POST",
-				path :"/upload",
-				headers : {
-				"Content-Type":"multipart/form-data; boundary="+ boundary,
-				"Content-Length": formStr.length + datas.length + formEnd.length
-				}
-			};
-
-			var req = http.request(options, function(res) {
-				res.on("data", function(data) {
-					console.log("返回数据"+ data);
-				});
-			});
-
-			req.write(formStr);
-			req.write(datas);
-			req.write(formEnd);
-			req.end();
-		}
+	function result(img,blogid){
+			//各类设置  
+		var opt={  
+		   // "url":"http://120.26.67.221:8081/LabHomeAdmin/commonajax/fileupload.do?blogid="+blogid+"&pictype=ordinaryImage",//url  
+		    "url":"http://localhost:3003/upload",
+		    "file":"./public/fly_order_btn.png",//文件位置  
+		    "param":"filedata",//文件上传字段名  
+		    "boundary":"----WebKitFormBoundary"+getBoundary()  
+		}  
+		  
+		postRequest(opt);  
+	}
 });
 
 
 router.post("/upload",function(req,res,next){
-	var url=req.body.blogid;
-	var blogid=req.body.pictype;
-	console.log(url,blogid);
-	res.send({
-		"status":"ok"
-	})
+	console.log("upload start")
+	
+  var form = new formidable.IncomingForm();   //创建上传表单
+    form.encoding = 'utf-8';        //设置编辑
+    form.uploadDir = 'tmp';    //设置上传目录
+    form.keepExtensions = true;  //保留后缀
+    form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
+
+  form.parse(req, function(err, fields, files) {
+console.log(files.filedata);
+    if (err) {
+      res.locals.error = err;
+      res.render('index', { title: TITLE });
+      return;       
+    }  
+     
+    var extName = '';  //后缀名
+    switch (files.filedata.type) {
+      case 'image/pjpeg':
+        extName = 'jpg';
+        break;
+      case 'image/jpeg':
+        extName = 'jpg';
+        break;       
+      case 'image/png':
+        extName = 'png';
+        break;
+      case 'image/x-png':
+        extName = 'png';
+        break;       
+    }
+
+    if(extName.length == 0){
+        res.locals.error = '只支持png和jpg格式图片';
+        res.render('index', { title: TITLE });
+        return;                
+    }
+
+    var avatarName = Math.random() + '.' + extName;
+    var newPath = form.uploadDir + avatarName;
+
+    console.log(newPath);
+    fs.renameSync(files.filedata.path, newPath);  //重命名
+  });
+
+  res.locals.success = '上传成功';
+  res.render('index', { title: "qw" });      
 })
+
+
+//post值payload  
+var getfield=function(field, value) {  
+    return 'Content-Disposition: form-data; name="'+field+'" '+value+' ';  
+}  
+  
+//文件payload  
+var getfieldHead=function (field, filename) {  
+    var fileFieldHead='Content-Disposition: form-data; name="'+field+'"; filename="'+filename+'"\r\n'+'Content-Type: '+getMime(filename)+'\r\n\r\n';  
+    return fileFieldHead;  
+}  
+//获取Mime  
+var getMime=function (filename) {  
+    var mimes = {  
+        '.png': 'image/png',  
+        '.gif': 'image/gif',  
+        '.jpg': 'image/jpeg',  
+        '.jpeg': 'image/jpeg',  
+        '.js': 'appliction/json',  
+        '.torrent': 'application/octet-stream'  
+    };  
+    var ext = path.extname(filename);  
+    var mime = mimes[ext];  
+    mime=!!mime?mime:'application/octet-stream';  
+    return mime;  
+}  
+//获取边界检查随机串  
+var getBoundary=function() {  
+    var max = 9007199254740992;  
+    var dec = Math.random() * max;  
+    var hex = dec.toString(36);  
+    var boundary = hex;  
+    return boundary;  
+}  
+//获取boundary  
+var getBoundaryBorder=function (boundary) {  
+    return '--'+boundary+'\r\n';  
+}  
+//字段格式化  
+function fieldPayload(opts) {  
+    var payload=[];  
+    for(var id in opts.field){  
+        payload.push(getfield(id,opts.field[id]));  
+    }  
+    console.log("payload",payload);
+    payload.push("");  
+    return payload.join(getBoundaryBorder(opts.boundary));  
+}  
+  
+//post数据  
+function postRequest (opts) {  
+    filereadstream(opts,function (buffer) {  
+        var options=require('url').parse(opts.url);  
+        var Header={};  
+        var h=getBoundaryBorder(opts.boundary);  
+        var e=fieldPayload(opts);  
+        var a=getfieldHead(opts.param,opts.file);  
+        var d="\r\n"+h;  
+        Header["Content-Length"]=Buffer.byteLength(h+e+a+d)+buffer.length;  
+        Header["Content-Type"]='multipart/form-data; boundary='+opts.boundary;  
+        options.headers=Header;  
+        options.method='POST';  
+        console.log(a);
+        var req=http.request(options,function(res){  
+            var data='';  
+            res.on('data', function (chunk) {  
+                data+=chunk;  
+            });  
+            res.on('end', function () {  
+                console.log(res.statusCode)  
+                console.log(data);  
+            });  
+        });  
+        req.write(h+e+a);/*log.diy(h+e+a+buffer+d);  */
+        req.write(buffer);  
+        req.end(d);  
+    });  
+      
+}  
+//读取文件  
+function filereadstream(opts, fn) {  
+    var readstream = fs.createReadStream(opts.file,{flags:'r',encoding:null});  
+    var chunks=[];  
+    var length = 0;  
+    readstream.on('data', function(chunk) {  
+        length += chunk.length;  
+        chunks.push(chunk);  
+    });  
+
+        console.log(opts);
+    readstream.on('end', function() {  
+        var buffer = new Buffer(length);  
+        for(var i = 0, pos = 0, size = chunks.length; i < size; i++) {  
+            chunks[i].copy(buffer, pos);  
+            pos += chunks[i].length;  
+        }  
+        fn(buffer);  
+    });  
+}  
+  
+
 
 module.exports = router;
