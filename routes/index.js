37b68cad3454f4d3c112c292f5a72fede12415c5
hanangjie <hanangjie@gmail.com>
2016-01-15 14:13:15 +0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -82,36 +82,67 @@ router.get('/img', function(req, res, next) {
 
                     res.on("end", function(){
                        
-                        fs.writeFile("./public/logonew"+random+"."+type, imgData, "binary", function(err){
+                        fs.writeFile("./tmp/logonew"+random+"."+type, imgData, "binary", function(err){
                             if(err){
                                 console.log("down fail");
                             }
                             console.log("imgget success");
-                            result("./public/logonew"+random+"."+type,blogid);
+                            result("./tmp/logonew"+random+"."+type,blogid);
                             
                         });
                     });
             }).on('error', function(e) {
                 console.log("下载失败: " + e.message);
             });
-          
-	
 
-/*result("./public/logonew."+type,blogid);*/
+
+
 	function result(img,blogid){
 			//各类设置  
 		var opt={  
-		    //"url":"http://120.26.67.221:8081/LabHomeAdmin/commonajax/fileupload.do?blogid="+blogid+"&pictype=ordinaryImage",//url  
+		    "url":"http://192.168.108.1:8080/LabHomeAdmin/commonajax/fileupload.do?blogid="+blogid+"&pictype=ordinaryImage",//url  
 		    //"url":"http://120.26.67.221:8083/LabHomeUpload/upload/useravatars",
-            "url":"http://localhost:3003/upload",
+            //"url":"http://localhost:3003/upload",
 		    "file":img,//文件位置  
 		    "param":"filedata",//文件上传字段名  
 		    "boundary":"----WebKitFormBoundary"+getBoundary()  
 		}  
 		  
-		postRequest(opt);  
-
+		//postRequest(opt);  
+		
+		//测试用例
+		var files = [
+		 {urlKey: "filedata", urlValue: img}
+		];
+		var options = { 
+		 host: "120.26.67.221",
+		 port: "8081" ,
+		 method: "POST", 
+		 path: "/LabHomeAdmin/commonajax/fileupload.do?blogid="+blogid+"&pictype=ordinaryImage"
+		};
+		 
+		var req = http.request(options, function(res){
+             console.log("RES:" + res);
+             console.log('STATUS: ' + res.statusCode);
+             console.log('HEADERS: ' + JSON.stringify(res.headers));
+             //res.setEncoding("utf8");
+             res.on("data", function(chunk){
+                console.log("BODY:" + chunk);
+                 sendback(chunk,img);
+             })
+		});
+		 
+		req.on('error', function(e){
+		 console.log('problem with request:' + e.message);
+		 console.log(e);
+		});
+		postFile(files, req);
+		console.log("done");
 	}
+    function sendback(data,img){
+        res.send(data);
+        fs.unlinkSync(img)
+    }
 });
 
 
@@ -167,114 +198,158 @@ router.post("/upload",function(req,res,next){
           });  
           });
             
-})
+});
 
 
-//post值payload  
-var getfield=function(field, value) {  
-    return 'Content-Disposition: form-data; name="'+field+'" '+value+' ';  
-}  
-  
-//文件payload  
-var getfieldHead=function (field, filename) {  
-    var fileFieldHead='Content-Disposition: form-data; name="'+field+'"; filename="'+filename+'"\r\n'+'Content-Type: '+getMime(filename)+'\r\n\r\n';  
-    return fileFieldHead;  
-}  
-//获取Mime  
-var getMime=function (filename) {  
-    var mimes = {  
-        '.png': 'image/png',  
-        '.gif': 'image/gif',  
-        '.jpg': 'image/jpeg',  
-        '.jpeg': 'image/jpeg',  
-        '.js': 'appliction/json',  
-        '.torrent': 'application/octet-stream'  
-    };  
-    var ext = path.extname(filename);  
-    var mime = mimes[ext];  
-    mime=!!mime?mime:'application/octet-stream';  
-    return mime;  
-}  
-//获取边界检查随机串  
-var getBoundary=function() {  
-    var max = 9007199254740992;  
-    var dec = Math.random() * max;  
-    var hex = dec.toString(36);  
-    var boundary = hex;  
-    return boundary;  
-}  
-//获取boundary  
-var getBoundaryBorder=function (boundary) {  
-    return '\r\n'+boundary+'\r\n';  
-}  
-//字段格式化  
-function fieldPayload(opts) {  
-    var payload=[];  
-    for(var id in opts.field){  
-        payload.push(getfield(id,opts.field[id]));  
-    }  
-    payload.push("");  
+//post值payload
+var getfield=function(field, value) {
+    return 'Content-Disposition: form-data; name="'+field+'" '+value+' ';
+}
 
-    return payload.join(getBoundaryBorder(opts.boundary));  
-}  
-  
-//post数据  
-function postRequest (opts) {  
-    filereadstream(opts,function (buffer) {  
+//文件payload
+var getfieldHead=function (field, filename) {
+    var fileFieldHead='Content-Disposition: form-data; name="'+field+'"; filename="'+filename+'"\r\n'+'Content-Type: '+getMime(filename)+'\r\n\r\n';
+    return fileFieldHead;
+}
+//获取Mime
+var getMime=function (filename) {
+    var mimes = {
+        '.png': 'image/png',
+        '.gif': 'image/gif',
+        '.jpg': 'image/jpeg',
+        '.jpeg': 'image/jpeg',
+        '.js': 'appliction/json',
+        '.torrent': 'application/octet-stream'
+    };
+    var ext = path.extname(filename);
+    var mime = mimes[ext];
+    mime=!!mime?mime:'application/octet-stream';
+    return mime;
+}
+//获取边界检查随机串
+var getBoundary=function() {
+    var max = 9007199254740992;
+    var dec = Math.random() * max;
+    var hex = dec.toString(36);
+    var boundary = hex;
+    return boundary;
+}
+//获取boundary
+var getBoundaryBorder=function (boundary) {
+    return '\r\n----'+boundary+'--';
+}
+//字段格式化
+function fieldPayload(opts) {
+    var payload=[];
+    for(var id in opts.field){
+        payload.push(getfield(id,opts.field[id]));
+    }
+    payload.push("");
+
+    return payload.join(getBoundaryBorder(opts.boundary));
+}
+
+//post数据
+function postRequest (opts) {
+    filereadstream(opts,function (buffer) {
         console.log("post start")
-        var options=require('url').parse(opts.url);  
-        var Header={};  
-        var h=getBoundaryBorder(opts.boundary);  
-        var e=fieldPayload(opts);  
-        var a=getfieldHead(opts.param,opts.file);  
-        var d="\r\n"+h;  
-        Header["Content-Length"]=Buffer.byteLength(h+e+a+d)+buffer.length;  
-        Header["Content-Type"]='multipart/form-data; boundary='+opts.boundary;  
-        options.headers=Header;  
-        options.method='POST';  
+        var options=require('url').parse(opts.url);
+        var Header={};
+        var h=getBoundaryBorder(opts.boundary);
+        var e=fieldPayload(opts);
+        var a=getfieldHead(opts.param,opts.file);
+        //var d="\r\n"+h;
+        Header["Content-Length"]=Buffer.byteLength(h+e+a)+buffer.length;
+        Header["Content-Type"]='multipart/form-data; boundary='+opts.boundary;
+        options.headers=Header;
+        options.method='POST';
         console.log("options",options);
-        var req=http.request(options,function(res){  
+        var req=http.request(options,function(res){
             console.log('STATUS: ' + res.statusCode);
               console.log('HEADERS: ' + JSON.stringify(res.headers));
               res.setEncoding('utf8');
-            var data='';  
-            res.on('data', function (chunk) {  
-                data+=chunk;  
-            });  
-            res.on('end', function () {  
-                console.log(res.statusCode)  
-                console.log(data);  
-            });  
+            var data='';
+            res.on('data', function (chunk) {
+                data+=chunk;
+            });
+            res.on('end', function () {
+                console.log(res.statusCode)
+                console.log(data);
+            });
         }).on('error', function(e) {
               console.log("上传失败: " + e.message);
-            });  
+            });
         req.write(h+e+a);/*log.diy(h+e+a+buffer+d);  */
-        req.write(buffer);  
-        req.end(d);  
-    });  
-      
-}  
-//读取文件  
-function filereadstream(opts, fn) {  
-    var readstream = fs.createReadStream(opts.file,{flags:'r',encoding:null});  
-    var chunks=[];  
-    var length = 0;  
-    readstream.on('data', function(chunk) {  
-        length += chunk.length;  
-        chunks.push(chunk);  
-    });  
+        req.write(buffer);
+        //req.end();
+		req.flush();
+    });
+
+}
+//读取文件
+function filereadstream(opts, fn) {
+    var readstream = fs.createReadStream(opts.file,{flags:'r',encoding:null});
+    var chunks=[];
+    var length = 0;
+    readstream.on('data', function(chunk) {
+        length += chunk.length;
+        chunks.push(chunk);
+    });
 
         console.log(opts);
-    readstream.on('end', function() {  
-        var buffer = new Buffer(length);  
-        for(var i = 0, pos = 0, size = chunks.length; i < size; i++) {  
-            chunks[i].copy(buffer, pos);  
-            pos += chunks[i].length;  
-        }  
-        fn(buffer);  
-    });  
-}  
+    readstream.on('end', function() {
+        var buffer = new Buffer(length);
+        for(var i = 0, pos = 0, size = chunks.length; i < size; i++) {
+            chunks[i].copy(buffer, pos);
+            pos += chunks[i].length;
+        }
+        fn(buffer);
+    });
+}
   
-
-
+ //------------------------------测试方法-----------------------------//
+	
+	function postFile(fileKeyValue, req) {
+		  var boundaryKey = Math.random().toString(16);
+		  var enddata = '\r\n----' + boundaryKey + '--';
+		 
+		  var files = new Array();
+		  for (var i = 0; i < fileKeyValue.length; i++) {
+		   var content = "\r\n----" + boundaryKey + "\r\n" + "Content-Type: application/octet-stream\r\n" + "Content-Disposition: form-data; name=\"" + fileKeyValue[i].urlKey + "\"; filename=\"" + path.basename(fileKeyValue[i].urlValue) + "\"\r\n" + "Content-Transfer-Encoding: binary\r\n\r\n";
+		   var contentBinary = new Buffer(content, 'utf-8');//当编码为ascii时，中文会乱码。
+		   	files.push({contentBinary: contentBinary, filePath: fileKeyValue[i].urlValue});
+		  }
+		  var contentLength = 0;
+		  for (var i = 0; i < files.length; i++) {
+		   var stat = fs.statSync(files[i].filePath);
+		   contentLength += files[i].contentBinary.length;
+		   contentLength += stat.size;
+		  }
+		 
+		  req.setHeader('Content-Type', 'multipart/form-data;boundary=--' + boundaryKey);
+		  req.setHeader('Content-Length', contentLength + Buffer.byteLength(enddata));
+		 console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"+contentLength + Buffer.byteLength(enddata));
+		  // 将参数发出
+		  var fileindex = 0;
+		  var doOneFile = function(){
+		   req.write(files[fileindex].contentBinary);
+		   var fileStream = fs.createReadStream(files[fileindex].filePath, {bufferSize : 4 * 1024});
+		   fileStream.pipe(req, {end: false});
+		   fileStream.on('end', function() {
+		     fileindex++;
+		     if(fileindex == files.length){
+		      req.end(enddata);
+		     } else {
+		      doOneFile();
+		     }
+		   });
+		  };
+		  if(fileindex == files.length){
+		    req.end(enddata);
+		  } else {
+		    doOneFile();
+		  }      
+		}
+		 
+ 
 module.exports = router;
